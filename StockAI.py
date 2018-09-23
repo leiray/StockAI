4d8438c5a2d87b6d0b17cea80d69544b4956c44f
leiray <1946247544@qq.com>
2018-09-23 16:53:54 +0800

    SVN提交

--- StockAI.py
+++ StockAI.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2018/9/22 22:35
+# @Author : leifengchuan
+# @File : StockAI.py
+"""
+对外提供接口服务的文件
+
+
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
